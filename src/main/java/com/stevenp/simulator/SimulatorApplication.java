53304291d692c5bbe47e874146a9665bc4501c29
Steven Phan <ptviet@live.com>
2018-12-21 22:30:11 +1100

    initial setup

---
+++ src/main/java/com/stevenp/simulator/SimulatorApplication.java
@@ -0,0 +1,14 @@
+package com.stevenp.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimulatorApplication.class, args);
+  }
+
+}
+
