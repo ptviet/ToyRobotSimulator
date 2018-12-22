c60c651c902f2578118b9f0f88267aa63f176bf2
Steven Phan <ptviet@live.com>
2018-12-22 13:06:02 +1100

    add position change and getNextPosition

--- src/main/java/com/stevenp/simulator/simulator/Position.java
+++ src/main/java/com/stevenp/simulator/simulator/Position.java
@@ -1,5 +1,7 @@
 package com.stevenp.simulator.simulator;
 
+import com.stevenp.simulator.exception.ToyRobotException;
+
 public class Position {
   private int x;
   private int y;
@@ -9,6 +11,40 @@ public class Position {
     this.x = x;
     this.y = y;
     this.direction = direction;
+  }
+
+  private Position(Position position) {
+    this.x = position.getX();
+    this.y = position.getY();
+    this.direction = position.getDirection();
+  }
+
+  private void change(int x, int y) {
+    this.x = this.x + x;
+    this.y = this.y + y;
+  }
+
+  public Position getNextPosition() throws ToyRobotException {
+    if (this.direction == null)
+      throw new ToyRobotException("Invalid robot direction");
+
+    // new position to validate
+    Position newPosition = new Position(this);
+    switch (this.direction) {
+      case NORTH:
+        newPosition.change(0, 1);
+        break;
+      case EAST:
+        newPosition.change(1, 0);
+        break;
+      case SOUTH:
+        newPosition.change(0, -1);
+        break;
+      case WEST:
+        newPosition.change(-1, 0);
+        break;
+    }
+    return newPosition;
   }
 
   public int getX() {
---
+++ src/test/java/com/stevenp/simulator/simulator/PositionTest.java
@@ -0,0 +1,30 @@
+package com.stevenp.simulator.simulator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PositionTest {
+
+  @Test
+  public void testGetNextPosition() throws Exception {
+    Position position = new Position(0, 0, Direction.EAST);
+
+    Position newPosition = position.getNextPosition();
+    Assert.assertEquals(newPosition.getX(), 1);
+    Assert.assertEquals(newPosition.getY(), 0);
+    Assert.assertEquals(newPosition.getDirection(), Direction.EAST);
+
+    newPosition = newPosition.getNextPosition();
+    Assert.assertNotEquals(newPosition.getX(), 1);
+    Assert.assertEquals(newPosition.getY(), 0);
+    Assert.assertEquals(newPosition.getDirection(), Direction.EAST);
+
+    newPosition.setDirection(Direction.NORTH);
+    newPosition = newPosition.getNextPosition();
+    Assert.assertNotEquals(newPosition.getX(), 1);
+    Assert.assertEquals(newPosition.getY(), 1);
+    Assert.assertNotEquals(newPosition.getDirection(), Direction.EAST);
+
+  }
+
+}
